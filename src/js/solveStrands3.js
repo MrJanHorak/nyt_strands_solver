40999a8a1d71ba1da84e5b1283e910ae129160e1
MrJanHorak <janny_b_goode@hotmail.com>
2024-04-10 21:01:08 -0400

    refactor coordinateTrie and try a genetic algorithm to find solution

--- src/js/solveStrands3.js
+++ src/js/solveStrands3.js
@@ -267,248 +267,279 @@ function findCompleteWordSets(words) {
 const completeWordSets = findCompleteWordSets(foundWords.entries());
 console.log('Complete Word Sets:', completeWordSets);
 
-// Function to find maximum filled word sets
-function findMaxFilledWordSets(words) {
-  let maxSetSize = 0;
-  const completeSets = [];
+// // Function to find maximum filled word sets
+// function findMaxFilledWordSets(words) {
+//   let maxSetSize = 0;
+//   const completeSets = [];
 
-  for (const [word1, coords1] of words) {
-    const usedLetters = new Set(word1.split(''));
-    let overlaps = false;
+//   for (const [word1, coords1] of words) {
+//     const usedLetters = new Set(word1.split(''));
+//     let overlaps = false;
 
-    for (const set of completeSets) {
-      for (const word2 of set) {
-        if (shareCoordinates(coords1, word2[1])) {
-          overlaps = true;
-          break;
-        }
-      }
-      if (overlaps) break;
-    }
-
-    if (!overlaps) {
-      const currentSet = new Set([word1]);
-      for (const [word2, coords2] of words) {
-        if (word1 !== word2 && !shareCoordinates(coords1, coords2)) {
-          const word2UsedLetters = new Set(word2.split(''));
-          let allNewLetters = true;
-          for (const letter of word2UsedLetters) {
-            if (usedLetters.has(letter)) {
-              allNewLetters = false;
-              break;
-            }
-          }
-
-          if (allNewLetters) {
-            currentSet.add(word2);
-            usedLetters.add(...word2UsedLetters);
-          }
-        }
-
-        // Check for early termination based on max set size
-        if (currentSet.size === maxSetSize) {
-          break; // No larger set possible for this word (word1)
-        }
-      }
-
-      // Update max set size if current set is larger
-      if (currentSet.size > maxSetSize) {
-        maxSetSize = currentSet.size;
-      }
-
-      // Add set only if it's the maximum size
-      if (currentSet.size === maxSetSize) {
-        completeSets.push([...currentSet]);
-      }
-    }
-  }
-
-  return completeSets;
-}
-
-// Find maximum filled word sets
-const maxFilledSets = findMaxFilledWordSets(foundWords.entries());
-console.log('Maximum Filled Word Sets:', maxFilledSets);
-
-// Function to find all possible non-overlapping word sets
-function findAllNonOverlappingWordSets(words) {
-  const completeSets = [];
-
-  for (const [word1, coords1] of words) {
-    const usedLetters = new Set(word1.split(''));
-    let overlaps = false;
-
-    for (const set of completeSets) {
-      for (const word2 of set) {
-        if (shareCoordinates(coords1, word2[1])) {
-          overlaps = true;
-          break;
-        }
-      }
-      if (overlaps) break;
-    }
-
-    if (!overlaps) {
-      const currentSet = new Set([word1]);
-      for (const [word2, coords2] of words) {
-        if (word1 !== word2 && !shareCoordinates(coords1, coords2)) {
-          const word2UsedLetters = new Set(word2.split(''));
-          let allNewLetters = true;
-          for (const letter of word2UsedLetters) {
-            if (usedLetters.has(letter)) {
-              allNewLetters = false;
-              break;
-            }
-          }
-
-          if (allNewLetters) {
-            currentSet.add(word2);
-            usedLetters.add(...word2UsedLetters);
-          }
-        }
-      }
-
-      // Add all valid sets regardless of size
-      completeSets.push([...currentSet]);
-    }
-  }
-
-  return completeSets;
-}
-
-// Find all possible non-overlapping word sets
-const allSets = findAllNonOverlappingWordSets(foundWords.entries());
-console.log('All Possible Non-Overlapping Word Sets:', allSets);
-
-// console.log('All Words (without coordinates):');
-// for (const word of foundWords.keys()) {
-//   console.log(word);
-// }
-
-// function findOptimalNonOverlappingWords(words) {
-//   const wordsArray = Array.from(words.entries());
-//   let bestSolution = [];
-
-//   function backtrack(startIndex = 0, currentSolution = []) {
-//     // If the current solution is better than the best solution, update the best solution
-//     if (currentSolution.length > bestSolution.length) {
-//       bestSolution = currentSolution;
+//     for (const set of completeSets) {
+//       for (const word2 of set) {
+//         if (shareCoordinates(coords1, word2[1])) {
+//           overlaps = true;
+//           break;
+//         }
+//       }
+//       if (overlaps) break;
 //     }
 
-//     for (let i = startIndex; i < wordsArray.length; i++) {
-//       const [word, coords] = wordsArray[i];
+//     if (!overlaps) {
+//       const currentSet = new Set([word1]);
+//       for (const [word2, coords2] of words) {
+//         if (word1 !== word2 && !shareCoordinates(coords1, coords2)) {
+//           const word2UsedLetters = new Set(word2.split(''));
+//           let allNewLetters = true;
+//           for (const letter of word2UsedLetters) {
+//             if (usedLetters.has(letter)) {
+//               allNewLetters = false;
+//               break;
+//             }
+//           }
 
-//       // Check if the current word overlaps with any word in the current solution
-//       let overlaps = currentSolution.some(([_, existingCoords]) =>
-//         shareCoordinates(coords, existingCoords)
-//       );
+//           if (allNewLetters) {
+//             currentSet.add(word2);
+//             usedLetters.add(...word2UsedLetters);
+//           }
+//         }
 
-//       // If the current word does not overlap with any word in the current solution, add it to the current solution and continue the search
-//       if (!overlaps) {
-//         backtrack(i + 1, [...currentSolution, [word, coords]]);
+//         // Check for early termination based on max set size
+//         if (currentSet.size === maxSetSize) {
+//           break; // No larger set possible for this word (word1)
+//         }
+//       }
+
+//       // Update max set size if current set is larger
+//       if (currentSet.size > maxSetSize) {
+//         maxSetSize = currentSet.size;
+//       }
+
+//       // Add set only if it's the maximum size
+//       if (currentSet.size === maxSetSize) {
+//         completeSets.push([...currentSet]);
 //       }
 //     }
 //   }
 
-//   backtrack();
-
-//   const optimalNonOverlappingWords = new Map(bestSolution);
-//   console.log('Optimal Non-overlapping Words:', optimalNonOverlappingWords.size);
-//   console.log('Optimal Non-overlapping Words:', optimalNonOverlappingWords.entries());
-//   return optimalNonOverlappingWords;
+//   return completeSets;
 // }
 
-// function findOptimalNonOverlappingWords(words) {
-//   const wordsArray = Array.from(words.entries()).sort((a, b) => b[0].length - a[0].length);
-//   let bestSolution = [];
+// // Find maximum filled word sets
+// const maxFilledSets = findMaxFilledWordSets(foundWords.entries());
+// console.log('Maximum Filled Word Sets:', maxFilledSets);
 
-//   function backtrack(startIndex = 0, currentSolution = []) {
-//     // If the current solution is better than the best solution, update the best solution
+// // Function to find all possible non-overlapping word sets
+// function findAllNonOverlappingWordSets(words) {
+//   const completeSets = [];
+
+//   for (const [word1, coords1] of words) {
+//     const usedLetters = new Set(word1.split(''));
+//     let overlaps = false;
+
+//     for (const set of completeSets) {
+//       for (const word2 of set) {
+//         if (shareCoordinates(coords1, word2[1])) {
+//           overlaps = true;
+//           break;
+//         }
+//       }
+//       if (overlaps) break;
+//     }
+
+//     if (!overlaps) {
+//       const currentSet = new Set([word1]);
+//       for (const [word2, coords2] of words) {
+//         if (word1 !== word2 && !shareCoordinates(coords1, coords2)) {
+//           const word2UsedLetters = new Set(word2.split(''));
+//           let allNewLetters = true;
+//           for (const letter of word2UsedLetters) {
+//             if (usedLetters.has(letter)) {
+//               allNewLetters = false;
+//               break;
+//             }
+//           }
+
+//           if (allNewLetters) {
+//             currentSet.add(word2);
+//             usedLetters.add(...word2UsedLetters);
+//           }
+//         }
+//       }
+
+//       // Add all valid sets regardless of size
+//       completeSets.push([...currentSet]);
+//     }
+//   }
+
+//   return completeSets;
+// }
+
+// // Find all possible non-overlapping word sets
+// const allSets = findAllNonOverlappingWordSets(foundWords.entries());
+// console.log('All Possible Non-Overlapping Word Sets:', allSets);
+
+// function findOptimalNonOverlappingWords(words) {
+//   const wordsArray = Array.from(words.entries()).sort(
+//     (a, b) => b[0].length - a[0].length
+//   );
+//   let bestSolution = [];
+//   const trie = new CoordinateTrie();
+
+//   function backtrack(
+//     startIndex = 0,
+//     currentSolution = [],
+//     trie = new CoordinateTrie()
+//   ) {
 //     if (currentSolution.length > bestSolution.length) {
 //       bestSolution = currentSolution;
 //     }
 
-//     // If adding all remaining words won't make a better solution, stop searching
-//     if (currentSolution.length + wordsArray.length - startIndex <= bestSolution.length) {
+//     // if (currentSolution.length + wordsArray.length - startIndex <= bestSolution.length) {
+//     //   return;
+//     // }
+
+//     if (currentSolution.length >= 6 && currentSolution.length <= 9) {
 //       return;
 //     }
 
 //     for (let i = startIndex; i < wordsArray.length; i++) {
 //       const [word, coords] = wordsArray[i];
 
-//       // Check if the current word overlaps with any word in the current solution
-//       let overlaps = currentSolution.some(([_, existingCoords]) =>
-//         shareCoordinates(coords, existingCoords)
-//       );
-
-//       // If the current word does not overlap with any word in the current solution, add it to the current solution and continue the search
-//       if (!overlaps) {
-//         backtrack(i + 1, [...currentSolution, [word, coords]]);
+//       if (!trie.hasOverlap(coords)) {
+//         const newTrie = new CoordinateTrie();
+//         for (const [existingWord, existingCoords] of currentSolution) {
+//           newTrie.insert(existingWord, existingCoords);
+//         }
+//         newTrie.insert(word, coords);
+//         backtrack(i + 1, [...currentSolution, [word, coords]], newTrie);
 //       }
 //     }
 //   }
-
 //   backtrack();
 
 //   const optimalNonOverlappingWords = new Map(bestSolution);
-//   console.log('Optimal Non-overlapping Words:', optimalNonOverlappingWords.size);
-//   console.log('Optimal Non-overlapping Words:', optimalNonOverlappingWords.entries());
+//   console.log(
+//     'Optimal Non-overlapping Words:',
+//     optimalNonOverlappingWords.size
+//   );
+//   console.log(
+//     'Optimal Non-overlapping Words:',
+//     optimalNonOverlappingWords.entries()
+//   );
 //   return optimalNonOverlappingWords;
 // }
 
 // const optimalNonOverlappingWords = findOptimalNonOverlappingWords(foundWords);
 // console.log('Optimal Non-overlapping Words:', optimalNonOverlappingWords);
 
-function findOptimalNonOverlappingWords(words) {
-  const wordsArray = Array.from(words.entries()).sort(
-    (a, b) => b[0].length - a[0].length
-  );
-  let bestSolution = [];
-  const trie = new CoordinateTrie();
 
-  function backtrack(
-    startIndex = 0,
-    currentSolution = [],
-    trie = new CoordinateTrie()
-  ) {
-    if (currentSolution.length > bestSolution.length) {
-      bestSolution = currentSolution;
-    }
+const POPULATION_SIZE = foundWords.size;
+const MAX_GENERATIONS = 10000;
 
-    // if (currentSolution.length + wordsArray.length - startIndex <= bestSolution.length) {
-    //   return;
-    // }
-
-    if (currentSolution.length >= 6 && currentSolution.length <= 9) {
-      return;
-    }
-
-    for (let i = startIndex; i < wordsArray.length; i++) {
-      const [word, coords] = wordsArray[i];
-
-      if (!trie.hasOverlap(coords)) {
-        const newTrie = new CoordinateTrie();
-        for (const [existingWord, existingCoords] of currentSolution) {
-          newTrie.insert(existingWord, existingCoords);
-        }
-        newTrie.insert(word, coords);
-        backtrack(i + 1, [...currentSolution, [word, coords]], newTrie);
-      }
-    }
-  }
-  backtrack();
-
-  const optimalNonOverlappingWords = new Map(bestSolution);
-  console.log(
-    'Optimal Non-overlapping Words:',
-    optimalNonOverlappingWords.size
-  );
-  console.log(
-    'Optimal Non-overlapping Words:',
-    optimalNonOverlappingWords.entries()
-  );
-  return optimalNonOverlappingWords;
+// Initialization
+let population = [];
+for (let i = 0; i < POPULATION_SIZE; i++) {
+  const individual = Array.from({length: Math.floor(Math.random() * 10)}, () => {
+    const wordsArray = Array.from(foundWords.keys());
+    return wordsArray[Math.floor(Math.random() * wordsArray.length)];
+  });
+  population.push(individual);
 }
 
-const optimalNonOverlappingWords = findOptimalNonOverlappingWords(foundWords);
-console.log('Optimal Non-overlapping Words:', optimalNonOverlappingWords);
+// Fitness Function
+function fitness(individual) {
+  let totalCoverage = 0;
+  for (const word of individual) {
+    const coords = foundWords.get(word);
+    totalCoverage += coords.length;
+  }
+  return totalCoverage;
+}
+
+// Selection
+function selection(population) {
+  const fitnesses = population.map(fitness);
+  const totalFitness = fitnesses.reduce((a, b) => a + b, 0);
+  const rand = Math.random() * totalFitness;
+  let sum = 0;
+  for (let i = 0; i < population.length; i++) {
+    sum += fitnesses[i];
+    if (sum >= rand) {
+      return population[i];
+    }
+  }
+  return population[population.length - 1];
+}
+
+// Crossover
+function crossover(parent1, parent2) {
+  const crossoverPoint = Math.floor(Math.random() * parent1.length);
+  let child = [...parent1.slice(0, crossoverPoint), ...parent2.slice(crossoverPoint)];
+
+  // Remove overlapping words
+  const trie = new CoordinateTrie();
+  child = child.filter(word => {
+    const coords = foundWords.get(word);  // Retrieve the coordinates for the word
+    if (!trie.hasOverlap(word, coords)) {  // Pass the coordinates to hasOverlap
+      trie.insert(word, coords);
+      return true;
+    }
+    return false;
+  });
+
+  return child;
+}
+
+// Mutation
+function mutate(individual) {
+  const wordsArray = Array.from(foundWords.keys());
+  const numMutations = Math.floor(Math.random() * individual.length);
+
+  // Create a trie from the individual
+  const trie = new CoordinateTrie();
+  for (const word of individual) {
+    const coords = foundWords.get(word);
+    trie.insert(word, coords);
+  }
+
+  for (let i = 0; i < numMutations; i++) {
+    const index = Math.floor(Math.random() * individual.length);
+    let newWord;
+    let coords;
+
+    // Find a new word that does not overlap with the existing words
+    do {
+      newWord = wordsArray[Math.floor(Math.random() * wordsArray.length)];
+      coords = foundWords.get(newWord);
+    } while (trie.hasOverlap(newWord, coords));
+
+    // Replace the word at the selected index with the new word
+    const oldCoords = foundWords.get(individual[index]);
+    trie.delete(individual[index], oldCoords);
+    trie.insert(newWord, coords);
+    individual[index] = newWord;
+  }
+
+  return individual;
+}
+// Main GA loop
+for (let generation = 0; generation < MAX_GENERATIONS; generation++) {
+  let newPopulation = [];
+  for (let i = 0; i < POPULATION_SIZE; i++) {
+    const parent1 = selection(population);
+    const parent2 = selection(population);
+    let child = crossover(parent1, parent2);
+    child = mutate(child);
+    newPopulation.push(child);
+  }
+  population = newPopulation;
+console.log('Generation:', generation, 'Best Fitness:', population.reduce((a, b) => fitness(a) > fitness(b) ? a : b));
+  // Check if a solution that covers all cells has been found
+  const bestIndividual = population.reduce((a, b) => fitness(a) > fitness(b) ? a : b);
+  if (fitness(bestIndividual) === rows * cols) {
+    console.log('Solution found:', bestIndividual);
+    break;
+  }
+}
--- src/js/trieCoordinates.js
+++ src/js/trieCoordinates.js
@@ -2,12 +2,14 @@ class CoordinateTrieNode {
   constructor() {
     this.children = new Map();
     this.wordEnd = false;
+    this.words = new Set();
   }
 }
 
 class CoordinateTrie {
   constructor() {
     this.root = new CoordinateTrieNode();
+    this.trie = new Map();  // Move this line to here
   }
 
   insert(word, coords) {
@@ -19,9 +21,13 @@ class CoordinateTrie {
       node = node.children.get(coord);
     }
     node.wordEnd = true;
+    node.words.add(word);
   }
 
-  hasOverlap(coords) {
+  hasOverlap(word, coords) {
+    if (!coords || typeof coords[Symbol.iterator] !== 'function') {
+      throw new Error('coords must be an iterable');
+    }
     let node = this.root;
     for (const coord of coords) {
       if (!node.children.has(coord)) {
@@ -29,7 +35,25 @@ class CoordinateTrie {
       }
       node = node.children.get(coord);
     }
+    node.words.delete(word);
     return true;
+  }
+
+  delete(word, coords) {
+    // Convert the coordinates to a string to use as a key
+    const key = coords.map((coord) => coord.join(',')).join(';');
+
+    // Remove the word from the trie
+    const wordsAtKey = this.trie.get(key);
+    if (wordsAtKey) {
+      const index = wordsAtKey.indexOf(word);
+      if (index !== -1) {
+        wordsAtKey.splice(index, 1);
+      }
+      if (wordsAtKey.length === 0) {
+        this.trie.delete(key);
+      }
+    }
   }
 }
 
