a4374739262a662b5a1a2a4f91e07eec06d93e1f
MrJanHorak <janny_b_goode@hotmail.com>
2024-06-14 21:24:28 -0400

    add a spangram functionality

--- src/components/FoundWords/FoundWords.jsx
+++ src/components/FoundWords/FoundWords.jsx
@@ -1,14 +1,66 @@
+import { useState, useEffect } from 'react';
 import './FoundWords.css';
 
 function FoundWords({ FoundWords }) {
+  const [spanGram, setSpanGram] = useState([]);
+  const [spanGramIndex, setSpanGramIndex] = useState(0);
+
+  const handleAddtoSpanGram = (word) => {
+    setSpanGram([...spanGram, word]);
+    setSpanGramIndex(spanGramIndex + 1);
+  };
+
+  const handleRemoveFromSpanGram = (word) => {
+    setSpanGram(spanGram.filter((w) => w !== word));
+    setSpanGramIndex(spanGramIndex - 1);
+  };
+
+  const handleOnClick = (word) => {
+    if (spanGram.includes(word)) {
+      handleRemoveFromSpanGram(word);
+    } else {
+      handleAddtoSpanGram(word);
+    }
+  };
+
+  useEffect(() => {}, []);
+
   return (
     <div className='found-words'>
       <h4>Found Words {FoundWords.length}</h4>
-      <div className="found-word-list">
+      <div className='found-word-list'>
         {FoundWords.map((word, index) => {
-          return <div className='found-word' key={index}>{word.word}</div>;
+          if (!spanGram.includes(word.word)) {
+            return (
+              <div
+                className='found-word'
+                key={index}
+                onClick={() => handleOnClick(word.word)}
+              >
+                {word.word}
+              </div>
+            );
+          }
         })}
-    </div>
+      </div>
+      {spanGram.length > 0 && (
+        <div className='span-gram'>
+          <h4>Span Gram</h4>
+          <div className='span-gram-list'>
+            {spanGram.map((word, index) => {
+              return (
+                <div
+                  className='spangram-word'
+                  onClick={() => handleOnClick(word)}
+                  key={index}
+                >
+                  {word}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
